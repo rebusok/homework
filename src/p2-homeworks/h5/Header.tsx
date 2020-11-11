50d4082d9f05e9d290e7c17b163afb13797c5ded
Yuriy <rebusok@yandex.ru>
2020-11-11 13:51:46 +0300

    fin home_5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,12 +3,14 @@ import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
 function Header() {
     return (
-        <div className={s.dropbox}> <span className={s.dropbox_btn}>Menu</span>
-            <nav className={s.dropbox_list}>
-                <NavLink exact to="/pre-junior" activeClassName={s.active}>Pre-junior</NavLink>  
-                <NavLink to="/junior" activeClassName={s.active}>Junior</NavLink>  
-                <NavLink to="/junior_plus" activeClassName={s.active}>JuniorPlus</NavLink>
-            </nav>
+        <div className={s.dropbox_head}>        
+            <div className={s.dropbox}> <span className={s.dropbox_btn}>Menu</span>
+                <nav className={s.dropbox_list}>
+                    <NavLink exact to="/pre-junior" activeClassName={s.active}>Pre-junior</NavLink>  
+                    <NavLink to="/junior" activeClassName={s.active}>Junior</NavLink>  
+                    <NavLink to="/junior_plus" activeClassName={s.active}>JuniorPlus</NavLink>
+                </nav>
+            </div>
         </div>
     );
 }
