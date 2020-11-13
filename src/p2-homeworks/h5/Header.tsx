07b9abadf601e7632a6c9ef7676da0319e82c8c5
Yuriy <rebusok@yandex.ru>
2020-11-13 14:43:11 +0300

    add path

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import s from './Header.module.css';
+import {PATH}  from './Routes';
 function Header() {
     return (
         <div className={s.dropbox_head}>        
             <div className={s.dropbox}> <span className={s.dropbox_btn}>Menu</span>
                 <nav className={s.dropbox_list}>
-                    <NavLink exact to="/pre-junior" activeClassName={s.active}>Pre-junior</NavLink>  
-                    <NavLink to="/junior" activeClassName={s.active}>Junior</NavLink>  
-                    <NavLink to="/junior_plus" activeClassName={s.active}>JuniorPlus</NavLink>
+                    <NavLink exact to={PATH.PRE_JUNIOR} activeClassName={s.active}>Pre-junior</NavLink>  
+                    <NavLink to={PATH.JUNIOR} activeClassName={s.active}>Junior</NavLink>  
+                    <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>JuniorPlus</NavLink>
                 </nav>
             </div>
         </div>
