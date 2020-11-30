8f602c56503720957f752301e645b9da15ab07ca
Yuriy <rebusok@yandex.ru>
2020-11-30 14:52:11 +0300

    finish 9Leson

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,46 +1,85 @@
 import React, {useState} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
-
+import s from './Clock.module.css';
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
     const [date, setDate] = useState<Date>();
     const [show, setShow] = useState<boolean>(false);
-
+    const [showData, setShowData] = useState<boolean>(false);
     const stop = () => {
-        // stop
+
+        clearInterval(timerId);
     }
+
     const start = () => {
         stop();
+
         const id: number = window.setInterval(() => {
-            // setDate
+            const date = new Date();
+
+            setDate(date)
+            setShow(true);
+
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
-        // show
+        setShowData(true)
     };
     const onMouseLeave = () => {
-        // close
+        setShowData(false)
     };
+    const dayss =  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const zeroAdd = (num:any):any => {
+        if (num < 10) {
+           return  '0' + num
+        } else {
+            return  num
+        }
+    }
 
+    let hours:any = zeroAdd(date?.getHours()),
+        minutes:any = zeroAdd(date?.getMinutes()),
+        seconds:any = zeroAdd(date?.getSeconds()),
+        days:any = date?.getDay(),
+        ears:any = date?.getFullYear(),
+        month:any = zeroAdd(date?.getMonth()),
+        nbrDay:any = date?.getDate();
+
+    if (date?.getMonth()) {
+       month += 1
+    }
+
+console.log(ears);
+    console.log(nbrDay);
+    const stringTime = `${hours}:${minutes}:${seconds}`; // fix with date
+    const stringDate = ` ${ears}-${month}-${nbrDay} : ${dayss[days]}`; // fix with date
+    console.log(stringTime)
+    const finalClassName = `${s.wrapper}  ${show ?  s.displayOn : s.displayOff}`
     return (
         <div>
-            <div
+            <div className={finalClassName}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
+                <div className={s.clock}>
+                    {show && (
+                        <div className={s.time}>
+                            {stringTime}
+                        </div>
+                    )}
+                    {showData && (
+                        <div className={s.date}>
+                            {stringDate}
+                        </div>
+                    )}
+                </div>
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
