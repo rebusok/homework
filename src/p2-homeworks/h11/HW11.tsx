cfdc1be555f68cb4defde9a238c61f092138146b
Yuriy <rebusok@yandex.ru>
2020-12-04 15:57:58 +0300

    range

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,21 +1,22 @@
 import React, {useState} from "react";
 import SuperRange from "./common/c7-SuperRange/SuperRange";
 import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
+import WrappAcc from "../../p1-main/m1-ui/u1-app/Hoc/layout/wrappAcc/WrappAcc";
 
 function HW11() {
     const [value1, setValue1] = useState(0);
     const [value2, setValue2] = useState(100);
 
     return (
-        <div>
-            <hr/>
-            homeworks 11
+        <WrappAcc title={'homeworks 11'}>
+
+
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
                 <SuperRange
-                    // сделать так чтоб value1 изменялось
+                    onChangeRange={setValue1}
                 />
             </div>
 
@@ -27,12 +28,12 @@ function HW11() {
                 <span>{value2}</span>
             </div>
 
-            <hr/>
+
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
+
+        </WrappAcc>
     );
 }
 
