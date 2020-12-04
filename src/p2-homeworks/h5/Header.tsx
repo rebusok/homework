424482bd327032c54447c1378e5aa3d892497784
Yuriy <rebusok@yandex.ru>
2020-12-04 15:47:51 +0300

    fix links

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -11,13 +11,25 @@ function Header(props:any) {
     if (!props.isOpen){
         cls.push(s.close)
     }
+    const clickHandler = () => {
+        props.onClose()
+    }
     return (
         <React.Fragment>
                 <nav className={cls.join(' ')}>
                     <div className={s.wrapper}>
-                        <NavLink exact to={PATH.PRE_JUNIOR} activeClassName={s.active}>Pre-junior</NavLink>
-                        <NavLink to={PATH.JUNIOR} activeClassName={s.active}>Junior</NavLink>
-                        <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>JuniorPlus</NavLink>
+                        <NavLink
+                            exact to={PATH.PRE_JUNIOR}
+                            activeClassName={s.active}
+                            onClick={clickHandler}>Pre-junior</NavLink>
+                        <NavLink
+                            to={PATH.JUNIOR}
+                            activeClassName={s.active}
+                            onClick={clickHandler}>Junior</NavLink>
+                        <NavLink
+                            to={PATH.JUNIOR_PLUS}
+                            activeClassName={s.active}
+                            onClick={clickHandler}>JuniorPlus</NavLink>
                     </div>
                 </nav>
             {props.isOpen ? <BackDrop onClick={props.onClose}/> : null}
